e3cbdd47dbbfbbacfa574512d5348d793b58e0e9
Jose <jrios.rubio23@gmail.com>
2017-11-25 22:59:24 +0100

    Github format fix

--- src/rfscomm.h
+++ src/rfscomm.h
@@ -1,15 +1,15 @@
 #ifndef RFSCOMM_h
     #define RFSCOMM_h
 
-	/***********************************************************************************/
+    /***********************************************************************************/
 
     #include "utility/AVR_Crypto_Lib_aes/aes.h"
     #include "utility/Manchester_Lib/manchester.h"
 
-	/***********************************************************************************/
+    /***********************************************************************************/
 
-    #define AES_KEY_SIZE    32  // AES-256 encryption Key size (32 bytes - 256 bits)
-    #define AES_BLOCK_SIZE  16  // AES-256 encryption Data block size (16 bytes - 128 bits)
+    #define AES_KEY_SIZE     32 // AES-256 encryption Key size (32 bytes - 256 bits)
+    #define AES_BLOCK_SIZE   16 // AES-256 encryption Data block size (16 bytes - 128 bits)
     #define RX_TIMEOUT      100 // Reception waiting time
     #define NO_USE          255 // No use value for Tx or Rx pins
 
@@ -19,7 +19,7 @@
         18,19,20,21,22,23,24,25,26,27,28,29,30,31
     };
 
-	/***********************************************************************************/
+    /***********************************************************************************/
 
     class RFSCOMM
     {
@@ -27,8 +27,8 @@
             RFSCOMM(uint8_t my_id);
             void config(uint8_t _tx_pin, uint8_t _rx_pin, uint8_t rf_bauds);
             void send(uint8_t dest_id, uint8_t data);
-			void send(uint8_t dest_id, uint8_t cmd, uint8_t val);
-			void send(uint8_t dest_id, uint8_t* data, uint8_t data_length);
+	    void send(uint8_t dest_id, uint8_t cmd, uint8_t val);
+	    void send(uint8_t dest_id, uint8_t* data, uint8_t data_length);
             uint8_t receive();
             uint8_t rx_src_id();
             uint8_t rx_dev_id();
@@ -45,6 +45,6 @@
             uint8_t _waiting_rx;
     };
 
-	/***********************************************************************************/
+    /***********************************************************************************/
 
 #endif
